ced697da5cb87cbe5aa47cd0f900e4ab0c84cfc6
Daniel Howells <daniel.howells@gmail.com>
2014-06-05 16:35:05 +0100

    Update picture.rb
    Fix default_scope for use in Rails 4 (Support for calling #default_scope without a block is removed)

--- app/models/activeadmin_settings/picture.rb
+++ app/models/activeadmin_settings/picture.rb
@@ -65,7 +65,7 @@ module ActiveadminSettings
       include PictureMethods
 
       # Scopes
-      default_scope order('created_at desc')
+      default_scope { order('created_at desc') }
     end
   end
 end
