f109488ae41b1ba39706b0a209566824c6931fd1
Yuriy Chernov <yurgon.com@gmail.com>
2015-08-09 16:22:52 +0300

    Update 20121123145237_create_activeadmin_settings_settings.rb
    For html need more than 255 charters

--- db/migrate/20121123145237_create_activeadmin_settings_settings.rb
+++ db/migrate/20121123145237_create_activeadmin_settings_settings.rb
@@ -2,7 +2,7 @@ class CreateActiveadminSettingsSettings < ActiveRecord::Migration
   def change
     create_table :activeadmin_settings_settings do |t|
       t.string :name
-      t.text   :string
+      t.text   :text
       t.string :file
 
       t.timestamps
